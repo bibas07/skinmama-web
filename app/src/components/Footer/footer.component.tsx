ab812cce21eb96ebf9ca24a8a390c4bd300552ec
Bibas Koirala <koeralabibas@gmail.com>
2025-04-27 00:38:42 +0545

    Fix: footer

--- app/src/components/Footer/footer.component.tsx
+++ app/src/components/Footer/footer.component.tsx
@@ -1,18 +1,8 @@
 export function Footer() {
   return (
-    <footer className="bg-secondary text-white py-4 mt-6">
-      <div className="grid md:grid-cols-3 gap-8 grid-cols-1">
-        <div className="container mx-auto text-center">
-          <p>
-            &copy; {new Date().getFullYear()} SkinMama. All rights reserved.
-          </p>
-        </div>
-        <div className="container mx-auto text-center">
-          <p>Privacy Policy</p>
-        </div>
-        <div className="container mx-auto text-center">
-          <p>Terms and Conditions</p>
-        </div>
+    <footer className="bg-secondary text-white py-4">
+      <div className="container mx-auto text-center">
+        <p>&copy; {new Date().getFullYear()} SkinMama. All rights reserved.</p>
       </div>
     </footer>
   );
