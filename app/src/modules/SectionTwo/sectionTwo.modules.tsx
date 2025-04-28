f3228213b20825dc4c7a25d63fc85e88012b85fe
Bibas Koirala <koeralabibas@gmail.com>
2025-04-28 22:23:10 +0545

    Fix: process name

--- app/src/modules/SectionTwo/sectionTwo.modules.tsx
+++ app/src/modules/SectionTwo/sectionTwo.modules.tsx
@@ -4,6 +4,11 @@ export default function SkinMamaLifeCycle() {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen w-full bg-gray-100 overflow-x-hidden py-8 px-4">
       <div className="w-full max-w-7xl mx-auto">
+        <div className="flex w-full justify-center mb-5">
+          <h1 className="text-3xl font-bold tracking-tight sm:text-4xl">
+            How Does It <span className="text-primary text-5xl">Work ?</span>
+          </h1>
+        </div>
         <SDLCVisualization />
       </div>
     </div>
