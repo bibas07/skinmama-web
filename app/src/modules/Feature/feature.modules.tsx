b91b671fb5206a53528f4de6efb2a4cd9cd2da11
Bibas Koirala <koeralabibas@gmail.com>
2025-04-28 19:37:51 +0545

    Fix: added heading

--- app/src/modules/Feature/feature.modules.tsx
+++ app/src/modules/Feature/feature.modules.tsx
@@ -1,3 +1,14 @@
 import FeatureSection from '../../components/Feature/features.components';
 
-export default FeatureSection;
+export function Feature() {
+  return (
+    <div className="mt-6">
+      <div className="flex w-full justify-center">
+        <h1 className="text-3xl font-bold tracking-tight sm:text-4xl">
+          What Does We <span className="text-primary text-5xl">Offer ?</span>
+        </h1>
+      </div>
+      <FeatureSection />
+    </div>
+  );
+}
